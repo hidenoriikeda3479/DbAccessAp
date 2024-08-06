1d970973ed79c586a8d4cbdab2e2743b1b9bd45b
HIKEDA\hideh <hidenoriike81@gmail.com>
2024-08-06 14:44:40 +0900

    DBアクセスサンプル作成

---
+++ DbAccessAP/Form1.cs
@@ -0,0 +1,31 @@
+using DbAccessAP.Common;
+using System.Data;
+using System.Windows.Forms;
+
+namespace DbAccessAP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 検索ボタンクリックイベント
+        /// </summary>
+        /// <param name="sender">コントロール情報</param>
+        /// <param name="e">イベント情報</param>
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            DatabaseHelper dbHelper = new DatabaseHelper("接続文字列");
+
+            using (var connection = dbHelper.OpenConnection())
+            {
+                string sql = "SELECT EmployeeID, Name, Department, Position FROM Employees";
+                DataTable employeeData = dbHelper.ExecuteQuery(connection, sql);
+                dataGridView1.DataSource = employeeData;
+            }
+        }
+    }
+}
